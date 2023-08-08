d44a2839834398cc229826bf96011a0582a95730
sani4618 <sanijoseph2018@gmail.com>
2023-08-08 23:57:56 +0530

    Header part is completed

---
+++ src/component/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/details">Personal Details</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header

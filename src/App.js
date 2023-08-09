ce612031594d4edbba43fc8216ab1471ecba9f65
sani4618 <sanijoseph2018@gmail.com>
2023-08-09 09:51:19 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import PersonalDetails from './component/PersonalDetails';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 
 function App() {
   return (
     <div>
 
-      <PersonalDetails/>
+      <BrowserRouter>
+      <Router>
+        <Route path="/"exact element={<PersonalDetails/>}/>
+        <Route path="/details" exact element={<BookingApp/>}/>
+      </Router>
+      </BrowserRouter>
+
+
     
     </div>
   );
--- src/component/PersonalDetails.jsx
+++ src/component/PersonalDetails.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const PersonalDetails = () => {
   return (
     <div>
+        <Header/>
 
         <div className="container">
             <div className="row">
